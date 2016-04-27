1892ce1dc25970f661ebda24b60ba1ddad63c7c0
FELIPE <felipegomezite@gmail.com>
2016-04-26 21:56:21 -0500

    turismo mejorado

--- app/src/main/java/com/luisfelipegomezc/practicarionegro/Main3Activity.java
+++ app/src/main/java/com/luisfelipegomezc/practicarionegro/Main3Activity.java
@@ -3,7 +3,9 @@ package com.luisfelipegomezc.practicarionegro;
 import android.app.FragmentManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +22,33 @@ public class Main3Activity extends AppCompatActivity {
         mViewPager = (ViewPager) findViewById(R.id.pager);
         PagerAdapter adapter=new PagerAdapter(getSupportFragmentManager());
         mViewPager.setAdapter(adapter);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+        ActionBar.TabListener tabListener = new ActionBar.TabListener() {
+            @Override
+            public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+
+            @Override
+            public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+            }
+        };
+
+        ActionBar.Tab tab= actionBar.newTab().setText("Parque").setTabListener(tabListener);
+        actionBar.addTab(tab);
+        tab = actionBar.newTab().setText("Puente").setTabListener(tabListener);
+        actionBar.addTab(tab);
+        tab = actionBar.newTab().setText("Rionegro").setTabListener(tabListener);
+        actionBar.addTab(tab);
     }
 
     public class PagerAdapter extends FragmentPagerAdapter{
